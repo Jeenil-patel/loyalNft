b7e66894c867e24d654589b2236ee1ba317ea5fa
Jeenil=patel <pateljeenilp17@gmail.com>
2022-03-12 13:56:56 +0530

    nav_initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import "./styles/Navbar.css"
-import './App.css';
+
 import Navbar from './components/navbar';
+
+
+
+
 function App() {
   return (
     <div>
-
       <Navbar/>
     </div>
   );
